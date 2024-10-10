3377d6f91707d37387d04da5c9400f260d80b28e
dhekdani123 <abaspermadani0@gmail.com>
2024-10-10 15:23:41 +0700

    Tugas Modul 2 - Rosyid

---
+++ modul-2/230441100107_ABAS PERMADANI/buttonGroup1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MOUDUL2;
+
+/**
+ *
+ * @author LENOVO
+ */
+class buttonGroup1 {
+
+    static Object getSelection() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
